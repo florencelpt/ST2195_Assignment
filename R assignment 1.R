9bdf6bbcd61c4145ba7807e00f27b5f86040d3d9
Florence <91524350+florencelpt@users.noreply.github.com>
2021-09-29 14:49:13 +0800

    Add files via upload

---
+++ R assignment 1.R
@@ -0,0 +1,23 @@
+# Assignment 1 part 2
+
+## Printing Integers
+
+### without loop
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+### with loop
+
+for (i in c(1:10)) {
+  print(i)}
+
+
+
---
+++ assignment1_part3.py
@@ -0,0 +1,25 @@
+
+"""
+Created on Wed Sep 29 13:40:59 2021
+
+@author: florence
+
+Assignment 1 part 3
+"""
+
+# Print integers from 1 to 10
+
+print(1,2,3,4,5,6,7,8,9,10)
+
+# Print with loop
+#method 1
+
+integers = (1,2,3,4,5,6,7,8,9,10)
+for x in integers:
+    print(x)
+    
+#method 2
+
+for i in range(1,11):
+    print(i)
+    
